abcf52f18709d80fc1368e09a818bb9f5fa0a61f
Zeynep Toy <zeyneptoy.dev@icloud.com>
2025-08-03 10:22:31 +0300

    add xcode project

---
+++ ClapFinder/ClapFinder/ClapFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClapFinderApp.swift
+//  ClapFinder
+//
+//  Created by Zeynep Toy on 3.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClapFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
